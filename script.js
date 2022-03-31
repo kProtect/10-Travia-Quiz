adca042b8667161f511d80a4aadd5c9d1ad1f091
unknown <kyujin0704@gmail.com>
2022-03-31 01:31:46 -0400

    start button,question /answer finished

--- script.js
+++ script.js
@@ -1,138 +1,199 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.querySelector(".choice-text"));
-console.log(choices);
+const startEl = document.getElementById("start")
+const startButton = startEl.querySelector("button")
+const question = document.getElementById("question-container")
+const questionsEl = document.getElementById('question')
+const answerEl = document.getElementById("answer-button")
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0 ;
-let availableQuestion = [];
+let timeEl = document.querySelector("time")
 
-let questions = [
+
+let shuffledQuestions, currentQuestionIndex
+
+
+startButton.addEventListener('click', startGame)
+answerEl.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    startEl.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random()- .5)
+    currentQuestionIndex = 0
+    question.classList.remove('hide')
+    setNextQuestion()
+}
+
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionsEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+        button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswers)
+        answerEl.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    answerEl.classList.remove('hide')
+    while (answerEl.firstChild) {
+        answerEl.removeChild(answerEl.firstChild)
+    }
+}
+
+function selectAnswers(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffledQuestions.length > currentQuestionIndex +1){
+        questionsEl.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element){
+     element.classList.remove('correct')
+     element.classList.remove('wrong')
+}
+
+const questions = [
     {
         question: "In which continent are Chile, Argentina and Brazil?",
-        choice1: "<North America>",
-        choice2: "<South America>",
-        choice3: "<Europe>",
-        choice4: "<Australasia>",
-        answer: 2
-    },
-    {
+        answers: 
+        [ 
+        {text:"North America", correct: false},
+        {text:"South America" , correct: true},
+        {text:"Europe" , correct:false},
+        {text:"Australasia" , correct:false}
+         ]
+ },
+    
+    { 
         question: "Which brand of soup featured in one of Andy Warhols most famous pop art pieces?",
-        choice1: "<Heinz>",
-        choice2: "<Campbells>",
-        choice3: "<Baxters>",
-        choice4: "<Knorr>",
-        answer: 2
-    },
+
+        answers:[
+       { text: "Heinz" , correct:false},
+        {text: "Campbells" , correct:true},
+        {text: "Baxters" , correct:false},
+        {text: "Knorr" , correct:false}
+    ]
+},
+        
     {
         question: "The Mad Hatter and the Cheshire Cat are characters in which famous book?",
-        choice1: "<Winne-the-Pooh>",
-        choice2: "<Charlotte's Web>",
-        choice3: "<Charlie and the Chocolate Factory>",
-        choice4: "<Alice in Wonderland>",
-        answer: 4
-    },
+        answers:
+         [ 
+        {text: "Winne-the-Pooh" , correct:false},
+        {text: "Charlotte's Web" , correct:false},
+        {text: "Charlie and the Chocolate Factory" , correct:false},
+        {text: "Alice in Wonderland" , correct:true}
+    ]
+        
+    }
+    ,
     {
         question: "What measurement scale is used to determine wind speed?",
-        choice1: "<Beaufort scale>",
-        choice2: "<Richter scale>",
-        choice3: "<Synoptic scale>", 
-        choice4: "<Gusting scale>",
-        answer: 1
-    
-    },
+        answers: 
+       [ 
+     {text: "Beaufort scale" , correct:true},
+     {text: "Richter scale" , correct:false},
+     {text: "Synoptic scale" , correct:false}, 
+    { text: "Gusting scale" , correct:false}
+    ]       
+    }
+    ,
+
     {
         question: "In which city were the 1992 Summer Olympics held?",
-        choice1: "<Atlanta>",
-        choice2: "<Barcelona>", 
-        choice3: "<Sydney>",
-        choice4: "<Seoul>",
-        answer: 2
-    },
+        answers: 
+       [ 
+        {text: "Atlanta" , correct:false},
+        {text: "Barcelona" , correct:true}, 
+        {text: "Sydney" , correct:false},
+        {text: "Seoul" , correct:false}
+    ]
+        
+    }
+    ,
     {
         question: "What other country, besides the US, uses the US dollar as its official currency?",
-        choice1: "<Ecuador>",
-        choice2: "<Canada>", 
-        choice3:"<Mexico>", 
-        choice4:"<United Kingdom>",
-        answer: 1
+        answers: 
+        [
+        {text: "Ecuador" , correct:true},
+        {text: "Canada" , correct:false}, 
+        {text:"Mexico" , correct:false}, 
+        {text: "United Kingdom" , correct:false}
+    ]
+        
     },
     {
         question: "How many sides does a Dodecahedron have??",
-        choice1: "<12>",
-        choice2: "<24>",
-        choice3: "<14>",
-        choice4: "<20>",
-        answer: 1
-    },
+        answers: 
+        [
+        {text: "12" , correct:true},
+        {text: "24" , correct:false},
+        {text: "14" , correct:false},
+        {text: "20" , correct:false}
+    ]
+}
+    ,
     {
         question: "The Statue of Liberty was a gift to the United States from which European country?",
-        choice1: "<Belgium>" , 
-        choice2: "<Germany>" , 
-        choice3: "<Spain>" , 
-        choice4: "<France>",
-        answer: 4
+
+        answers: 
+        [
+        {text: "Belgium" , correct:false} , 
+        {text: "Germany" , correct:false} , 
+        {text: "Spain" , correct:false} , 
+        {text: "France" , correct:true}
+    ]
+        
     },
     {
         question: "Which traditional Spanish dance originated in Andalusia and is recognised by UNESCO as a heritage of humanity?",
-        choice1: "<Sardana>",
-        choice2:  "<Tango>",
-        choice3: "<Flamenco>",
-        choice4: "<Paso Doble>",
-        answer: 3
+        answers: 
+       [ 
+           {text: "Sardana" , correct:false},
+        {text:  "Tango" , correct:false},
+        {text: "Flamenco" , correct:true},
+        {text: "Paso Doble" , correct:false}
+    ]
+        
     },
     {
         question: "The human body is made up of approximately how much water?",
-        choice1: "<40%>",
-        choice2: "<50%>",
-        choice3: "<60%>",
-        choice4: "<70%>",
-        answer: 3
+        answers:
+        [{text: "40%" , correct:false},
+        {text: "50%" , correct:false},
+        {text: "60%" , correct:true},
+        {text: "70%" , correct:false}]
+        
     },
 ]
-
-const CORRECT_BONUS = 1;
-const MAX_QUESTION = 1;
-
-startGame = () => {
-    questionCounter =0;
-    score = 0;
-    availableQuestion = [questions];
-    console.log(availableQuestion)
-    getNewQuestion();
-};
-
-getNewQuestion = () => {
-
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestion.length);
-        currentQuestion = availableQuestion[questionIndex];
-        question.innerText = currentQuestion.question;
-
-        choices.forEach (choice => {
-            const number = choice.dataset['number'];
-            choice.innerText = currentQuestion['choice' + number];
-        })
-        availableQuestion.splice(questionIndex, 1);
-
-        acceptingAnswers = true;
-};
-
-choices.forEach(choice => {
-    choices.addEventListener('click', e =>{
-        if(!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
-    });
-});
-
-startGame();
-     
-
-
-
+        
+    
